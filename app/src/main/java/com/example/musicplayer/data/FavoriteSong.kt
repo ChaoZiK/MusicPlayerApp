3428675ae5b03aa3cee632bc6cecdb455db673bc
ITITIU20344 <112528962+ngoctuanan@users.noreply.github.com>
2024-12-01 17:05:36 +0700

    Initial DAO, Database and model

---
+++ app/src/main/java/com/example/musicplayer/data/FavoriteSong.kt
@@ -0,0 +1,17 @@
+package com.example.musicplayer.data
+
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "favorites_table")
+data class FavoriteSong(
+  @PrimaryKey(autoGenerate = true) val id: Int = 0,
+  val songId: String,
+  val title: String,
+  val artist: String,
+  val albumName: String,
+  val duration: Long,
+  val coverImageUrl: String,
+  val addedTimestamp: Long
+)
